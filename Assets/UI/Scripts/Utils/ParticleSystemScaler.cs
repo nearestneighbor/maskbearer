fee139359dae02cd5dd5c9f5a9825b08c8e94c44
Alexandr Frolov <maligan@rambler.ru>
2021-09-12 15:06:31 +0300

    Fix ParticleSystemScaler; Fix FX_Ashes simulation space -> world

--- Assets/UI/Scripts/Utils/ParticleSystemScaler.cs
+++ Assets/UI/Scripts/Utils/ParticleSystemScaler.cs
@@ -6,13 +6,19 @@ public class ParticleSystemScaler : MonoBehaviour
     void Update()
     {
         var system = GetComponent<ParticleSystem>();
-        var systemShape = system.shape;
+        if (system != null)
+        {
+            // Position
+            system.transform.position = Camera.main.transform.position;
 
-        systemShape.shapeType = ParticleSystemShapeType.Rectangle;
-        systemShape.scale = new Vector3(
-            Camera.main.orthographicSize * 2 * Camera.main.aspect,
-            Camera.main.orthographicSize * 2,
-            1
-        );
+            // Size
+            var systemShape = system.shape;
+            systemShape.shapeType = ParticleSystemShapeType.Rectangle;
+            systemShape.scale = new Vector3(
+                Camera.main.orthographicSize * 2 * Camera.main.aspect,
+                Camera.main.orthographicSize * 2,
+                1
+            );
+        }
     }
 }
